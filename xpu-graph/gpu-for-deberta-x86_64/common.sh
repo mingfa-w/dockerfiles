2f9db0845ca2b6d0e63c386ece5bdd46177e6296
jiangzishan <jiangzishan@bytedance.com>
2025-03-19 19:06:56 +0800

    add gpu docker image for deberta on x86_64

---
+++ xpu-graph/gpu-for-deberta-x86_64/common.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+# common var
+type=$1
+target_arch=`arch`
+
+# image param
+version="0.1.0"
+domain="hub.byted.org"
+namespace="tritonx"
+name="$type-deberta-ascend8.0.rc3-ubuntu20.04-${target_arch}"
+tag="$version"
+image="$domain/$namespace/$name:$tag"
+APPS_PATH="/opt/apps"
+base_image="hub.byted.org/base/lab.pytorch:e2e65bf1f8af8e432f378648d4d2bb67"
+if [ $type == "devel" ]; then
+  name_devel=runtime-${name#*-}
+  base_image=$domain/$namespace/$name_devel:$tag
+fi
+echo ======= $base_image =======
+base_image_param=" --build-arg APPS_PATH=$APPS_PATH --build-arg ARCH=$target_arch --build-arg BASE_IMAGE=$base_image "
+
+# ascend param
+ASCEND_CANN="8.0.RC3.alpha003"
+ASCEND_BASE="/usr/local/Ascend"
+ascend_param=" --build-arg ASCEND_CANN=$ASCEND_CANN --build-arg ASCEND_BASE=$ASCEND_BASE "
+
+# proxy param
+http_proxy="http://sys-proxy-rd-relay.byted.org:8118"
+proxy_param=" --build-arg ftp_proxy=$http_proxy --build-arg http_proxy=$http_proxy --build-arg https_proxy=$http_proxy "
+
+# torch and torch_npu param
+# TORCH_PACKAGE="https://download.pytorch.org/whl/cpu/torch-2.3.1-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+TORCH_PACKAGE=2.3.1
+TORCH_NPU_PACKAGE="https://gitee.com/ascend/pytorch/releases/download/v6.0.rc3-pytorch2.3.1/torch_npu-2.3.1.post2-cp39-cp39-manylinux_2_17_aarch64.manylinux2014_aarch64.whl"
+if [ $target_arch == 'x86_64' ]; then
+  # TORCH_PACKAGE='https://download.pytorch.org/whl/cpu/torch-2.3.1%2Bcpu-cp39-cp39-linux_x86_64.whl#sha256=a3cb8e61ba311cee1bb7463cbdcf3ebdfd071e2091e74c5785e3687eb02819f9'
+  TORCH_NPU_PACKAGE='https://gitee.com/ascend/pytorch/releases/download/v6.0.0-pytorch2.3.1/torch_npu-2.3.1.post4-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl'
+fi
+
+torch_and_npu_param=" --build-arg TORCH_PACKAGE=$TORCH_PACKAGE --build-arg TORCH_NPU_PACKAGE=$TORCH_NPU_PACKAGE " 
---
+++ xpu-graph/gpu-for-deberta-x86_64/run.sh
@@ -0,0 +1,127 @@
+#!/bin/bash -xe
+script_dir=$(cd $(dirname $0); pwd)
+cur_dir=`pwd`
+cd ${script_dir}
+USER=tiger
+CONTAINER_NAME=$USER
+STOP=0
+PORT=8006
+CORE_NUM=0
+# TYPE=devel
+TYPE=runtime
+# 处理参数
+while getopts "su:p:n:t:" opt
+do
+    case $opt in
+        s)
+            echo "选项 -s(stop) 被设置"
+            STOP=1
+            ;;
+        u)
+            echo "选项 -u(user) 的值是 $OPTARG"
+            CONTAINER_NAME=$OPTARG
+            ;;
+        p)
+            echo "选项 -p(port) 的值是 $OPTARG"
+            PORT=$OPTARG
+            ;;            
+        n)
+            echo "选项 -n(core number) 的值是 $OPTARG"
+            CORE_NUM=$OPTARG
+            ;;
+        t)
+            echo "选项 -t(type) 的值是 $OPTARG"
+            TYPE=$OPTARG
+            ;;
+        \?)
+            echo "无效选项: -$OPTARG" >&2
+            exit 1
+            ;;
+    esac
+done
+. ./common.sh $TYPE
+
+# 防止容器名重复
+CONTAINER_NAME=$CONTAINER_NAME.$tag.$PORT.$CORE_NUM
+
+docker_in_docker=" -p $PORT:22 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):/bin/docker "
+
+docker_run_flag=" --gpus all "
+
+MOUNT_DIR=$HOME
+# MOUNT_DIR_ASCEND=" -v /data00:/data00 \
+#                 -v /data01:/data01 \
+#                 -v /data02:/data02 \
+#                 -v /data03:/data03 \
+#                 -v /data04:/data04 \
+#                 -v /data05:/data05 \
+#                 -v /data06:/data06 \
+#                 -v /data07:/data07 "
+
+GROUP=`id -g -n`
+GROUPID=`id -g`
+CMD="docker ps -aq -f name=^$CONTAINER_NAME\$ -f status=running"
+echo CMD=$CMD
+OLD_ID=`$CMD`
+echo ==== container name: $CONTAINER_NAME, OLD_ID: $OLD_ID ===
+
+if [[ $STOP == 1 ]]; then
+    echo === stop $CONTAINER_NAME
+    docker stop $CONTAINER_NAME
+    exit 0
+fi
+
+if [ -z "$OLD_ID" ]; then
+    CMD="docker run $docker_in_docker $docker_run_flag -t -d --name $CONTAINER_NAME $MOUNT_DIR_ASCEND -v $MOUNT_DIR:/host --tmpfs /tmp:exec --rm $image "
+    echo CMD = $CMD
+    ID=`$CMD`
+    # docker exec --user root $ID groupadd -f -g $GROUPID $GROUP
+    # docker exec --user root $ID adduser --shell /bin/bash --uid $UID --gecos '' --ingroup $GROUP --disabled-password --home /home/$USER --force-badname $USER
+    # #docker exec --user root $ID bash -c " echo $USER ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USER && chmod 0440 /etc/sudoers.d/$USER"
+    # userPasswd="8uhb9ijn"
+    # echo -e "$userPasswd\n$userPasswd" | docker exec --user root -i $ID passwd $USER
+    # docker exec --user root $ID usermod -aG sudo $USER
+    # docker exec --user root $ID bash -c " echo $USER ALL=\(root\) NOPASSWD:ALL > /etc/sudoers"
+    # docker exec --user root $ID bash -c ' mkdir ~/.ssh; mkdir /tmp/ccache; ln -s /tmp/ccache ~/.ccache'
+
+    docker exec --user $USER $ID bash -c "mkdir -p /home/$USER/.ssh"
+    if [ -f ~/.ssh/authorized_keys ]; then
+        docker cp ~/.ssh/authorized_keys $ID:/home/$USER/.ssh/
+    fi
+
+    if [ -f ~/.ssh/config ]; then
+        docker cp ~/.ssh/config $ID:/home/$USER/.ssh/
+    fi
+
+    if [ -f ~/.ssh/id_rsa.pub ]; then
+        docker cp ~/.ssh/id_rsa.pub $ID:/home/$USER/.ssh/
+    fi
+
+    if [ -f ~/.ssh/id_rsa ]; then
+        docker cp ~/.ssh/id_rsa $ID:/home/$USER/.ssh/
+    fi
+
+    if [ -f ~/.ssh/known_hosts ]; then
+        docker cp ~/.ssh/known_hosts $ID:/home/$USER/.ssh/
+    fi
+    
+    GIT_USER=`git config --get user.name`
+    GIT_EMAIL=`git config --get user.email`
+    docker exec --user $USER $ID bash -c "git config --global user.name \"$GIT_NAME\""
+    docker exec --user $USER $ID bash -c "git config --global user.email \"$GIT_EMAIL\""
+    #docker cp ./gitconfig $ID:/home/$USER/.ssh
+
+    # for ascend
+    docker exec --user root $ID bash -c " cat /root/.bashrc_ascend >> /home/$USER/.bashrc"
+    docker exec --user $USER $ID bash -c " mkdir -p /home/$USER/.triton"
+    docker exec --user root $ID bash -c " sshpass -p $userPasswd scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@localhost:/root/.triton/json /home/$USER/.triton/"
+    docker exec --user root $ID bash -c " sshpass -p $userPasswd scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r root@localhost:/root/.triton/nvidia /home/$USER/.triton/"
+
+    # golang config
+    if [ -f ~/.gitconfig ]; then
+        docker cp $HOME/.gitconfig $ID:/home/$USER/
+    fi
+fi
+cd ${cur_dir}
+docker exec -it --user $USER $CONTAINER_NAME bash
+# docker exec -it --user root $CONTAINER_NAME bash
